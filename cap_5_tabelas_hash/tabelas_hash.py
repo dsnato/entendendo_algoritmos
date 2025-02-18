d77ca68edde067ac664e8c3befb95276edd00764
Nato <120518462+dsnato@users.noreply.github.com>
2025-02-18 15:35:50 -0300

    Add files via upload

---
+++ cap_5_tabelas_hash/tabelas_hash.py
@@ -0,0 +1,11 @@
+votaram = {}
+def verifica_eleitor(nome):
+    if votaram.get(nome):
+        print('Mande embora!')
+    else:
+        votaram[nome] = True
+        print('Deixe votar!')
+
+verifica_eleitor('Tom')
+verifica_eleitor('Mike')
+verifica_eleitor('Mike')
